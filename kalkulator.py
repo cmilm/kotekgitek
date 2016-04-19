c7aa66701446efc3e03288c115910128d0105034
cmilm <milenka.email@gmail.com>
2016-04-19 19:49:33 +0200

    Dodaje nowy superfajny plik

---
+++ kalkulator.py
@@ -0,0 +1,17 @@
+def linia(plik):
+    l= plik.readline()
+    while (l != ""):
+        try:
+          yield int(l.rstrip('\n\r').split(separator)[kolumna])
+        except ValueError:
+            yield 0
+        l = plik.readline()
+
+
+separator = ','
+nazwa ="./phoneCalls.csv"
+kolumna = 3
+plik= open(nazwa, 'r')
+print(sum(linia(plik, separator, kolumna)))
+plik.close()
+
